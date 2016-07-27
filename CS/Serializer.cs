f4a12aef8f58ee8a650940abb2d09910c8512d3e
Omar Alhammadi <sequencek@gmail.com>
2016-07-27 12:03:24 +0400

    add serializer and sprite code bases

---
+++ CS/Serializer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Input.Touch;
+using Microsoft.Xna.Framework.Graphics;
+
+using CS;
+
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.Collision;
+using FarseerPhysics.Collision.Shapes;
+using FarseerPhysics.Common;
+using FarseerPhysics.Factories;
+using FarseerPhysics.Controllers;
+using FarseerPhysics;
+
+namespace CS.Components
+{
+
+}
---
+++ CS/Sprite.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Input.Touch;
+using Microsoft.Xna.Framework.Graphics;
+
+using CS;
+
+namespace CS.Components
+{
+	/*
+	 * The sprite class takes a texture and changes it's source rectangle to match specific animation frame
+	*/
+	class Sprite
+	{
+
+	}
+
+	/*
+	 * The sprite system holds animation data for each texture that the sprite class uses
+	*/
+	class SpriteSystem : ComponentSystem<Sprite>, ISysUpdateable
+	{
+		public SpriteSystem(State state) : base(state)
+		{
+		}
+
+		public void Update(Global G)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
